6f308c66917cedba6a8689729b3b3c212a6a1fc6
ritu-477 <ritujoharbishnoi@gmail.com>
2024-10-04 20:03:02 +0530

    spacing

--- assets/js/style.js
+++ assets/js/style.js
@@ -1,3 +1,14 @@
+const accordionbtn2 = document.querySelectorAll(".accordionbtn2")
+accordionbtn2.forEach((e) => {
+    e.addEventListener("click", () => {
+        const activeaccodion = document.querySelector(".active")
+        e.parentElement.classList.toggle("active")
+        activeaccodion && activeaccodion.classList.remove("active")
+
+    })
+
+});
+
 const mybutton = document.querySelector(".topBtn");
 const show = document.querySelector(".show")
 
